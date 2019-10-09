55fa57d2d65ffcbe9e2fbd2692331b889ae2dd26
Elkhan <elkhan.safarov10@yandex.ru>
2019-10-09 14:59:02 +0500

    Creating a datetimepicker plugin

---
+++ src/datetime/day.js
@@ -0,0 +1,34 @@
+import React, { PropTypes } from 'react';
+import classnames from 'classnames';
+
+export default class Day extends React.Component {
+
+	static propTypes = {
+		i: PropTypes.number.isRequired,
+		w: PropTypes.number.isRequired,
+		d: PropTypes.number.isRequired
+	};
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		const {i, d, w} = this.props;
+		const prevMonth = (w === 0 && i > 7);
+		const nextMonth = (w => 4 && i > 14);
+
+		const wrapperClasses = classnames({
+			'prev-month': prevMonth,
+			'next-month': nextMonth,
+			'current-day': !prevMonth && !nextMonth && (i === d),
+			'disabled': (prevMonth || nextMonth)
+		});
+
+		return (
+			<div className={wrapperClasses} {...this.props}>
+				{i}
+			</div>
+		);
+	}
+}
---
+++ src/datetime/time.js
@@ -0,0 +1,66 @@
+import React, { PropTypes } from 'react';
+import classnames from 'classnames'; 
+import InputSlider from 'react-input-slider';
+
+
+export default class Calendar extends React.Component {
+
+	static PropTypes = {
+		moment: PropTypes.any.isRequired,
+		classNames: PropTypes.string.isRequired,
+		onChange: PropTypes.func.isRequired
+	};
+
+	constructor(props) {
+		super(props); 
+
+		this.state = {
+			m: this.props.moment
+		}
+	}
+
+	changeHours(pos) {
+		const m = this.props.moment;
+		m.hour( parseInt(pos.x) );
+		this.setState({ m });
+		this.props.onChange(m);
+	}
+
+	changeMinutes(pos) {
+		const m = this.props.moment;
+		m.minutes( parseInt(pos.x) );
+		this.setState({ m });
+		this.props.onChange(m);
+	}
+
+	render() {
+		const { m } = this.state;
+		const wrapperClasses = classnames(this.props.classNames, 'tab-time'); 
+
+		return (
+			<div className={ wrapperClasses }>
+				<div className="showtime">
+					<span className="time">{ m.format('HH') }</span>
+					<span className="separator">:</span>
+					<span className="time">{ m.format('mm') }</span>
+				</div>
+				<div className="sliders">
+					<div className="time-text">Часы</div>
+					<InputSlider className="u-slider-time"
+						xmin={0}
+						xmax={23}
+						x={ m.hour() }
+						onChange={ this.changeHours.bind(this) }
+					/>
+					<div className="time-text">Минуты</div>
+					<InputSlider className="u-slider-time"
+						xmin={0}
+						xmax={59}
+						x={ m.minute() }
+						onChange={ this.changeMinutes.bind(this) }
+					/>
+				</div>
+			</div>
+		);
+	}
+}
